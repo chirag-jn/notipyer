a3c0ab67cc5980d0a17b7312780018fae2cec2ac
Chirag Jain <jainchg@gmail.com>
2021-05-29 03:10:41 +0530

    reformat setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,28 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+
+from notipyer import __version__ as version
+
+REQUIREMENTS = Path("./requirements.txt").read_text()
+
+long_description = ''
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup (
     name = 'notipyer',
-    version = '0.1.1',
+    version = version,
     description = 'Notification API for Python',
-    long_description = 'Async email notifications using SMTP',
+    long_description = long_description,
+    long_description_content_type="text/markdown",
     author = 'Chirag Jain',
+    url = 'https://github.com/chirag-jn/notipyer',
     packages = find_packages(),
-
-    install_requires = []
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires = REQUIREMENTS
 )
