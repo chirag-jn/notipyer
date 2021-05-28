44ae531978d4c305543ff31bced3117e77394b72
Chirag Jain <jainchg@gmail.com>
2021-05-29 03:00:01 +0530

    initialize setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup (
+    name = 'notipyer',
+    version = '0.1.1',
+    description = 'Notification API for Python',
+    long_description = 'Async email notifications using SMTP',
+    author = 'Chirag Jain',
+    packages = find_packages(),
+
+    install_requires = []
+)
