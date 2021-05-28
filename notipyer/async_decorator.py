824579cb1e85b0d38a52454e077876db79bd83a2
Chirag Jain <jainchg@gmail.com>
2021-05-29 03:33:22 +0530

    add async decorator

---
+++ notipyer/async_decorator.py
@@ -0,0 +1,50 @@
+import threading
+
+
+class TimeoutError(RuntimeError):
+    pass
+
+
+class AsyncCall(object):
+    def __init__(self, fnc, callback=None):
+        self.Callable = fnc
+        self.Callback = callback
+
+    def __call__(self, *args, **kwargs):
+        self.Thread = threading.Thread(
+            target=self.run,
+            name=self.Callable.__name__,
+            args=args, kwargs=kwargs)
+        self.Thread.start()
+        return self
+
+    def wait(self, timeout=None):
+        self.Thread.join(timeout)
+        if self.Thread.isAlive():
+            raise TimeoutError()
+        else:
+            return self.Result
+
+    def run(self, *args, **kwargs):
+        self.Result = self.Callable(*args, **kwargs)
+        if self.Callback:
+            self.Callback(self.Result)
+
+
+class AsyncMethod(object):
+    def __init__(self, fnc, callback=None):
+        self.Callable = fnc
+        self.Callback = callback
+
+    def __call__(self, *args, **kwargs):
+        return AsyncCall(self.Callable, self.Callback)(*args, **kwargs)
+
+
+def Async(fnc=None, callback=None):
+    if fnc is None:
+        def AddAsyncCallback(fnc):
+            return AsyncMethod(fnc, callback)
+
+        return AddAsyncCallback
+    else:
+        return AsyncMethod(fnc, callback)
