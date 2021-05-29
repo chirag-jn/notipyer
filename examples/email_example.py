95577c2dea0d406f6588a3ee81e57c9e487a4556
Chirag Jain <jainchg@gmail.com>
2021-05-30 00:28:24 +0530

    add email example

---
+++ examples/email_example.py
@@ -0,0 +1,13 @@
+from notipyer.email_notify import set_email_config, send_email
+
+SENDER_EMAIL = 'myemail@gmail.com'
+SENDER_PASS = 'password'
+set_email_config(SENDER_EMAIL, SENDER_PASS)
+
+subject = 'My Email Subject'
+body = 'My Email Body'
+to_recipients = ['to-email-1@domain.com', 'toemail2@domain.com']
+cc_recipients = ['cc-email-1@domain.com', 'cc-email-2@domain.com']
+bcc_recipients = ['bcc-email-1@domain.com', 'bcc-email-2@domain.com']
+
+send_email(subject, body, to_recipients, cc_recipients, bcc_recipients)
