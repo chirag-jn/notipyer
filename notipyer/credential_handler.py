0cd88173635d523b1b356dea7ef66f2799297134
Chirag Jain <jainchg@gmail.com>
2021-09-05 14:52:50 +0530

    add support for name in email

--- notipyer/credential_handler.py
+++ notipyer/credential_handler.py
@@ -1,15 +1,17 @@
 class credentials:
     EMAIL_ID = ''
     EMAIL_PASS = ''
+    EMAIL_USER = ''
     SLACK_TOKEN = ''
 
     def __init__(self):
         pass
 
 
-def _set_email_credentials(cred_obj, email, password):
+def _set_email_credentials(cred_obj, email, password, name):
     cred_obj.EMAIL_ID = email
     cred_obj.EMAIL_PASS = password
+    cred_obj.EMAIL_USER = name
 
 
 def _set_slack_token_credentials(cred_obj, token):
--- tests/test_email_send.py
+++ tests/test_email_send.py
@@ -1,6 +1,6 @@
 from credentials import *
 from notipyer.email_notify import send_email, set_email_config
 
-set_email_config(username, password)
+set_email_config(username, password, name)
 
 send_email("my_subject", "my_content", [target_email])
