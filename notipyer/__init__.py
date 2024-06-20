02403b520ba72dbd4c131f5ee9d337c84f3aca7f
Chirag Jain <jainchg@gmail.com>
2024-06-20 11:01:08 +0530

    bump version

--- notipyer/__init__.py
+++ notipyer/__init__.py
@@ -1,6 +1,6 @@
 from .credential_handler import credentials
 
-__version__ = '0.3.4'
+__version__ = '0.3.5'
 __name__ = 'Notipyer'
 __short_desc__ = 'Notification Triggers for Python'
 __url__ = 'https://github.com/chirag-jn/notipyer'
